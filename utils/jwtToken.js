86ff876e100f6abe1ba81145bec700d30a9217c0
Gaurav3697 <gm4063420@gmail.com>
2024-08-09 14:56:10 +0545

    first try to resolve cookie in production

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -7,8 +7,8 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + 15 * 60 * 60 * 1000 //When I uses process.env.COOKIE_EXPIRE insted of 5 it gives error,I has to find the solution
     ),
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    secure:process.env.NODE_ENV==="DEVELOPMENT"?false:true,
+    sameSite:process.env.NODE_ENV==="DEVELOPMENT"?"lax":"none",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
